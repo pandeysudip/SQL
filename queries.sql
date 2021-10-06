9a0a96d57d65123133bdfbdaaed95d8adb9ce234
pandeysudip <sudip@siu.edu>
2021-10-05 22:52:49 -0700

    updating files

--- queries.sql
+++ queries.sql
@@ -1,20 +1,70 @@
+-- 1.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees as e
 JOIN salaries as s
 ON e.emp_no=s.emp_no;
 
-
-
+-- 2.
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date=1986;
+WHERE hire_date=year(1986));
+
+--3.
+SELECT  d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name 
+FROM departments AS d
+JOIN dept_manager as dm
+ON d.dept_no=dm.dept_no
+JOIN employees as e
+ON dm.emp_no=e.emp_no;
 
 
+--4.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no=de.emp_no
+JOIN departments as d
+on de.dept_no=d.dept_no;
 
 
-
+--5.
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name='Hercules' AND last_name LIKE'B%';
+
+
+--6.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no=de.emp_no
+JOIN departments as d
+ON de.dept_no=d.dept_no
+WHERE d.dept_name='Sales';
+
+
+--7.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+ON e.emp_no=de.emp_no
+JOIN departments as d
+ON de.dept_no=d.dept_no
+WHERE d.dept_name='Sales' OR d.dept_name='Development';
+
+--8.
+SELECT last_name, COUNT(last_name) AS count
+FROM employees
+GROUP BY last_name
+ORDER BY count DESC;
+
+
+
+
+
+
+
+
+
 
 
--- tables.sql
+++ tables.sql
@@ -7,42 +7,43 @@ DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
 
-
 CREATE TABLE departments(
-          dept_no INT PRIMARY KEY NOT NULL,
+          dept_no CHAR(10) PRIMARY KEY NOT NULL,
           dept_name VARCHAR(50) NOT NULL
-)
+);
 
-
-CREATE TABLE dept_emp(
-            emp_no INT PRIMARY KEY NOT NULL,
-            dept_no INT REFERENCES departments(dept_no)
-)
-
-
-CREATE TABLE dept_manager(
-            dept_no INT REFERENCES departments(dept_no),
-            emp_no INT REFERENCES dept_emp(emp_no)
-)
-
+CREATE TABLE titles(
+            title_id CHAR(10) PRIMARY KEY,
+            title VARCHAR(20)
+); 
 
 CREATE TABLE employees(
-            emp_no INT REFERENCES dept_emp(emp_no),
-            emp_title_id INT NOT NULL,
+            emp_no INT PRIMARY KEY,
+            emp_title_id CHAR(10),
             birth_date DATE,
             first_name VARCHAR(20),
             last_name VARCHAR(20),
             sex CHAR,
             hire_date DATE
-)
+            FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
+);
+CREATE TABLE dept_emp(
+            emp_no INT REFERENCES employees(emp_no),
+            dept_no CHAR(10) REFERENCES departments(dept_no),
+            PRIMARY KEY(emp_no, dept_no)
+            
+);
+
+CREATE TABLE dept_manager(
+            dept_no CHAR(10) REFERENCES departments(dept_no),
+            emp_no INT  REFERENCES employees(emp_no)
+);
+
 
 CREATE TABLE salaries(
             emp_no INT REFERENCES employees(emp_no),
-            salary DEC
-)
+            salary INT
+);
 
 
-CREATE TABLE titles(
-            title_id INT REFERENCES employees(emp_title_id),
-            title VARCHAR(20)
-)
+
