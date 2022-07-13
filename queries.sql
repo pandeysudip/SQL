36df2a2a5e5bdff3eb02ff1d5b3083e220639df7
Sudip Pandey <sudip@siu.edu>
2022-07-13 10:42:51 -0700

    Updating queries

--- queries.sql
+++ queries.sql
@@ -59,13 +59,210 @@ FROM employees
 GROUP BY last_name
 ORDER BY count DESC;
 
+-- from online
+-- This is the final version of all queries
+-- Retirement eligibility & creating new table to hold info
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no
+WHERE dm.to_date = ('9999-01-01');
+
+-- Selecting current employees
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Employee list with gender and salary
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+-- Save into a new table
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+	ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+-- Add filters
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List of employees with departments
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+
+-- List of Sales employees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- INTO sales_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- List of employees in Sales and Development
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_dev
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development')
+ORDER BY ce.emp_no;
 
 
+/*Given the following data definition, write a query that returns the number of students whose first name is John. String comparisons should be case sensitive.
+TABLE students
+   id INTEGER PRIMARY KEY,
+   firstName VARCHAR(30) NOT NULL,
+   lastName VARCHAR(30) NOT NULL*/
+   
+select count(*)
+from students
+where firstName = 'John'
 
 
+/*A table containing the students enrolled in a yearly course has incorrect data in records with ids between 20 and 100 (inclusive).
+TABLE enrollments
+  id INTEGER NOT NULL PRIMARY KEY
+  year INTEGER NOT NULL
+  studentId INTEGER NOT NULL
+Write a query that updates the field 'year' of every faulty record to 2015.*/
+
+update enrollments
+set year = 2015
+where id between 20 and 100
 
 
+/*The following data definition defines an organization's employee hierarchy.
+An employee is a manager if any other employee has their managerId set to this employee's id. That means John is a manager if at least one other employee has their managerId set to John's id.
+TABLE employees
+  id INTEGER NOT NULL PRIMARY KEY
+  managerId INTEGER REFERENCES employees(id)
+  name VARCHAR(30) NOT NULL
+Write a query that selects only the names of employees who are not managers.*/
+
+select name
+from employees
+where id not in (select managerid from employees where managerid is Not NULL)
 
 
+/*Information about pets is kept in two separate tables:
+TABLE dogs
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+TABLE cats
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+Write a query that select all distinct pet names.*/
 
+select name from dogs
+union
+select name from cats
+
+
+/*Each item in a web shop belongs to a seller. To ensure service quality, each seller has a rating.
+The data are kept in the following two tables:
+TABLE sellers
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  rating INTEGER NOT NULL
+TABLE items
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  sellerId INTEGER REFERENCES sellers(id)
+Write a query that selects the item name and the name of its seller for each item that belongs to a seller with a rating greater than 4. The query should return the name of the item as the first column and name of the seller as the second column.*/
+
+select i.name, s.name
+from sellers s
+join items i
+on s.id = i.sellerId
+where rating >4
+
+
+/*A new social network site has the following data tables:
+USERS
+ID	NAME	SEX
+1	  Ann	  null
+2	  Steve	  m
+3	  Mary	  f
+4	  Brenda	f
+FRIENDS
+USER1	USER2
+1	    2
+1	    3
+2	    3
+Select data that will be returned by the following SQL query:*/
+
+SELECT users.name, COUNT(*) as count FROM users
+LEFT JOIN friends
+ON users.id = friends.user1 OR users.id = friends.user2
+WHERE users.sex = 'f'
+GROUP BY users.id, users.name;
 
