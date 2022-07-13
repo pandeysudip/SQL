f5bce0bfd8ab42bc08527c095cb90c1195c868fd
Sudip Pandey <sudip@siu.edu>
2022-07-13 10:44:05 -0700

    Sql queries

--- queries.sql
+++ queries.sql
@@ -176,7 +176,59 @@ FROM current_emp as ce
 WHERE d.dept_name IN ('Sales', 'Development')
 ORDER BY ce.emp_no;
 
+-- Number of [titles] retiring
+SELECT ce.emp_no,
+  ce.first_name,
+  ce.last_name,
+  ti.title,
+  ti.from_date,
+  ti.to_date
+INTO ret_titles
+FROM employees AS ce
+  INNER JOIN titles AS ti
+    ON (ce.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY ce.emp_no;
 
+SELECT * FROM ret_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM ret_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC;
+
+SELECT * FROM unique_titles
+
+-- Counting the number of each employee per title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles
+
+-- Creating a list of employees eligible for potential mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+  e.first_name,
+  e.last_name,
+  e.birth_date,
+  de.from_date,
+  de.to_date,
+  ti.title
+INTO mentorship
+FROM employees as e
+INNER JOIN dept_emp as de
+  ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+  ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship
 /*Given the following data definition, write a query that returns the number of students whose first name is John. String comparisons should be case sensitive.
 TABLE students
    id INTEGER PRIMARY KEY,
